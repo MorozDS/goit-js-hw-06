3a3321624f34867f995ea2cefbfc88eaa8a6f5bc
MorozDS <morozddima@gmail.com>
2022-02-15 23:03:00 +0200

    Исправил

--- js/task-05.js
+++ js/task-05.js
@@ -2,6 +2,10 @@ const inputText = document.querySelector('#name-input');
 const spanText = document.querySelector('#name-output');
 
 inputText.addEventListener('input', (event) => {
+
+    if (event.currentTarget.value === '') {
+        spanText.textContent = 'Anonymous';
+    } else
     spanText.textContent = event.currentTarget.value;
     
 });
--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,13 @@
 const textInput = document.querySelector('#validation-input');
+let inputLength = textInput.getAttribute('data-length')
+let num = Number(inputLength)
+
+
 textInput.addEventListener('blur', (event) => {
     let textLength = event.currentTarget.value.length
     console.log(textLength)
     
-    if (textLength !== 6) {
+    if (textLength !== num) {
         textInput.classList.add('invalid')
     } else  textInput.classList.replace('invalid', 'valid') 
 })
